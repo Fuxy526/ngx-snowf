f4b78b3786a78cb7585e1cce47da5881efb1e065
paigu <paigu@fenjiandan.com>
2017-12-19 16:32:30 +0800

    basic functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { SnowfComponent } from './snowf/snowf.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SnowfComponent
   ],
   imports: [
     BrowserModule
